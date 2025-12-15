b96b29953322068d099fd8eeb1107997a5466355
Joonatan Silvennoinen <joonatan.silvennoinen@hotmail.com>
2025-12-15 14:36:34 +0200

    Styled forms and add_room.

--- confetti_cuisine/controllers/homeController.js
+++ confetti_cuisine/controllers/homeController.js
@@ -5,48 +5,34 @@ var prices = [
     kind: "Villa",
     address: "Juokahaisenkatu 3 20540 Turku",
     number_of_room: 3,
-    price: 100
+    price: 100,
   },
   {
     kind: "Appartment",
     address: "linnankatu 3 20540 Turku",
     number_of_room: 2,
-    price: 50
+    price: 50,
   },
   {
     kind: "Room",
     address: "Yo-kylä 3 20540 Turku",
     number_of_room: 1,
-    price: 30
-  }
+    price: 30,
+  },
 ];
-
-/* exports.showCourses = (req, res) => {
-  res.render("courses", {
-    offeredCourses: courses
-  });
-};
-
-exports.showSignUp = (req, res) => {
-  res.render("contact");
-};
-
-exports.postedSignUpForm = (req, res) => {
-  res.render("thanks");
-}; */
 
 export default {
   showPrices: (req, res) => {
     res.render("prices", {
-      offeredPrices: prices
+      offeredPrices: prices,
     });
   },
 
-  showSignUp:(req, res) => {
+  showSignUp: (req, res) => {
     res.render("add_room");
   },
 
-  postedSignUpForm:(req, res) => {
+  postedSignUpForm: (req, res) => {
     res.render("thanks");
-  }
+  },
 };
